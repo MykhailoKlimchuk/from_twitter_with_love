4b0a5c51a196be7b85bb6793c2d609e9808deb9c
MykhailoKlimchuk <mikeklimchuck@gmail.com>
2019-10-06 18:53:18 +0300

    update

--- scraper/srcaper.py
+++ scraper/srcaper.py
@@ -3,14 +3,9 @@ import requests
 from urllib.request import urlopen
 from xml.etree import ElementTree as etree
 
-URL = "http://twitrss.me/twitter_user_to_rss/?user=genadiy_g&fetch=Fetch+RSS"
 
-with urlopen(URL, timeout=10) as r:
-    items = etree.parse(r).find('channel').findall('item')
-
-
-links = [item.find('link').text for item in items]
-print(links)
+'genadiy_g'
+URL_MASK = "http://twitrss.me/twitter_user_to_rss/?user={}&fetch=Fetch+RSS"
 
 
 def get_twit(link):
@@ -18,10 +13,23 @@ def get_twit(link):
     soup = BeautifulSoup(response.content, "html.parser")
     twits_text = soup.find(class_='TweetTextSize TweetTextSize--jumbo js-tweet-text tweet-text').text
     twits_text = twits_text.split('pic.twitter.com')[0] if 'pic.twitter.com' in twits_text else twits_text
-    print(twits_text)
+    return twits_text
 
 
-get_twit(links[0])
+def get_user_twits(user_name):
+    url = URL_MASK.format(user_name)
+    with urlopen(url, timeout=10) as r:
+        items = etree.parse(r).find('channel').findall('item')
 
-for link in links:
-    get_twit(link)
+    links = [item.find('link').text for item in items]
+
+    twits = []
+
+    for link in links:
+        twit = get_twit(link)
+        twits.append(twit)
+    return twits
+
+
+twits = get_user_twits('genadiy_g')
+print(twits)
