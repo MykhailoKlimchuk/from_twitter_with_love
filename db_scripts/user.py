a4c12cfccceb0efea397f14df97b8833302ac870
MykhailoKlimchuk <mikeklimchuck@gmail.com>
2019-10-07 00:05:57 +0300

    update

--- db_scripts/user.py
+++ db_scripts/user.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-engine = create_engine('sqlite:///db/db.sqlite', echo=True)
+engine = create_engine('sqlite:///db/db.sqlite', echo=False)
 
 
 class User(Base):
@@ -17,6 +17,48 @@ class User(Base):
         self.id = id_
         self.wants_to_subscribe = False
         self.wants_to_unsubscribe = False
+
+    def subscribe(self):
+        self.wants_to_subscribe = True
+        self.wants_to_unsubscribe = False
+
+    def unsubscribe(self):
+        self.wants_to_subscribe = False
+        self.wants_to_unsubscribe = True
+
+    def get_subscribe_status(self):
+        if self.wants_to_subscribe is True:
+            return True
+        if self.wants_to_unsubscribe is True:
+            return False
+
+    def process_subscribe_data(self, subscribe_id):
+        if self.get_subscribe_status() is True:
+            print(f'subscribe {subscribe_id}')
+        else:
+            print(f'unsubscribe {subscribe_id}')
+
+
+def get_user(user_id):
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+    exists_records = [game for game in session.query(User).filter(User.id == user_id)]
+    if len(exists_records) == 0:
+        print(111)
+        user = add_user(user_id, session)
+    else:
+        print(222)
+
+        user = exists_records[0]
+    session.commit()
+    return user
+
+
+def add_user(user_id, session):
+    user = User(user_id)
+    session.add(user)
+    return user
 
 
 def create_table():
--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -1,15 +1,19 @@
 import telebot
-
+from db_scripts import user as user_db
 
 with open('configs/token') as token_file:
     TOKEN = token_file.read()
 
 
 def subscribe(message):
+    user = user_db.get_user(message.from_user.id)
+    user.subscribe()
     bot.send_message(message.chat.id, 'Напиши ид юзера на кого хочешь подписаться пример @genadiy_g')
 
 
 def unsubscribe(message):
+    user = user_db.get_user(message.from_user.id)
+    user.unsubscribe()
     bot.send_message(message.chat.id, 'Напиши ид юзера от кого хочешь отписаться пример @genadiy_g')
 
 
@@ -26,13 +30,24 @@ keyboard1.row(*COMMANDS.keys())
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, я буду пересылать тебе твиты твоих подписок', reply_markup=keyboard1)
+    user = user_db.get_user(message.from_user.id)
+    response_message = f'Привет, {message.from_user.first_name} я буду пересылать тебе твиты твоих подписок'
+
+    bot.send_message(message.chat.id, response_message, reply_markup=keyboard1)
 
 
 @bot.message_handler(content_types=['text'])
 def button_click(message):
     button_cb = COMMANDS.get(message.text, None)
-    if button_cb is None:
+    user = user_db.get_user(message.from_user.id)
+
+    if button_cb is not None:
+        button_cb(message)
         return
 
-    button_cb(message)
+    if user.get_subscribe_status is not None:
+        user.process_subscribe_data(message.text)
+
+
+
+bot.polling()
